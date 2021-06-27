a874b5a294d6e452a9a9dfeca9d40caf9b4a11fe
Alexander A. Klimov <grandmaster@al2klimov.de>
2021-06-27 22:39:51 +0200

    Introduce Select()

--- dulldb.go
+++ dulldb.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/Al2Klimov/FUeL.go"
 	"github.com/natefinch/atomic"
+	"os"
 )
 
 // Replace saves the data $value into the database file $into (as with json.Marshal).
@@ -19,4 +20,19 @@ func Replace(into string, value interface{}) fuel.ErrorWithStack {
 	}
 
 	return fuel.AttachStackToError(atomic.WriteFile(into, buf), 0)
+}
+
+// Select loads the database file $from into *$to (as with json.Unmarshal).
+func Select(from string, to interface{}) fuel.ErrorWithStack {
+	f, err := os.Open(from)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+
+		return fuel.AttachStackToError(err, 0)
+	}
+	defer f.Close()
+
+	return fuel.AttachStackToError(json.NewDecoder(f).Decode(to), 0)
 }
--- dulldb_test.go
+++ dulldb_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"reflect"
 	"testing"
 )
 
@@ -35,5 +36,60 @@ func TestReplace(t *testing.T) {
 
 	if string(written) != persisted {
 		t.Errorf("Replace(%#v, %#v): written %#v, expected %#v", file, structure, string(written), persisted)
+	}
+}
+
+func TestSelect(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.RemoveAll(dir)
+
+	file := path.Join(dir, "db.json")
+	if err := ioutil.WriteFile(file, []byte(persisted), 0600); err != nil {
+		t.Error(err)
+		return
+	}
+
+	noPerm := path.Join(dir, "noperm.json")
+	if err := ioutil.WriteFile(noPerm, []byte(persisted), 0000); err != nil {
+		t.Error(err)
+		return
+	}
+
+	invalid := path.Join(dir, "invalid.json")
+	if err := ioutil.WriteFile(invalid, nil, 0600); err != nil {
+		t.Error(err)
+		return
+	}
+
+	noSuch := path.Join(dir, "nosuch.json")
+	const wontVanish = "won't vanish"
+	var data interface{} = wontVanish
+
+	if err := Select(noSuch, &data); err == nil {
+		if data != wontVanish {
+			t.Errorf("Select(%#v, %#v): changed var passed by pointer to %#v", noSuch, &data, data)
+		}
+	} else {
+		t.Errorf("Select(%#v, %#v): got %#v, expected nil", noSuch, &data, err)
+	}
+
+	if err := Select(file, &data); err == nil {
+		if !reflect.DeepEqual(data, structure) {
+			t.Errorf("Select(%#v, %#v): got %#v, expected %#v", noSuch, &data, data, structure)
+		}
+	} else {
+		t.Errorf("Select(%#v, %#v): got %#v, expected nil", file, &data, err)
+	}
+
+	if Select(noPerm, &data) == nil {
+		t.Errorf("Select(%#v, %#v): got nil, expected error", noPerm, &data)
+	}
+
+	if Select(invalid, &data) == nil {
+		t.Errorf("Select(%#v, %#v): got nil, expected error", invalid, &data)
 	}
 }
